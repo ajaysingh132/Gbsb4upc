076e9616bbdc5f40c6fb5770b9c63126f202a5bd
ajaysingh132 <banjarajanmat@gmail.com>
2025-12-30 20:29:36 +0530

    Implement WebView to load a URL in MainActivity

--- app/src/Java/webviewapp/Activity.java
+++ app/src/Java/webviewapp/Activity.java
@@ -1,1 +1,20 @@
+package com.example.webviewapp;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.activity.ComponentActivity;
+
+public class MainActivity extends ComponentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.loadUrl("https://example.com");
+        setContentView(webView);
+    }
+}
