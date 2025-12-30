ac7a6e077dc6233a54a0456af5d832da9d330604
ajaysingh132 <banjarajanmat@gmail.com>
2025-12-30 20:09:58 +0530

    Add Gradle Wrapper script for project setup

--- gradlew.bat
+++ gradlew.bat
@@ -1,1 +1,18 @@
+@echo off
+:: Apache License 2.0 (Gradle Wrapper script)
 
+SETLOCAL
+set DIR=%~dp0
+
+IF NOT "%JAVA_HOME%"=="" (
+  set JAVA_EXE=%JAVA_HOME%\bin\java.exe
+) ELSE (
+  set JAVA_EXE=java.exe
+)
+
+set CLASSPATH=%DIR%\gradle\wrapper\gradle-wrapper.jar
+
+set DEFAULT_JVM_OPTS=-Xmx1g -Dfile.encoding=UTF-8
+
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+ENDLOCAL
